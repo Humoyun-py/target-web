db4021b84ae1a19b81c5146034e717dafb81df0f
Humoyun-py <freif56@gmail.com>
2025-10-16 16:46:11 +0500

    navbera yangi

--- app.py
+++ app.py
@@ -368,5 +368,5 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
